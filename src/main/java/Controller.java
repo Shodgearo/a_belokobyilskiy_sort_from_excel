544de11138ec4e23b89bb07f6ae32dff00f814f8
Shodgearo <sanya.93.26@bk.ru>
2020-01-21 11:06:08 +0600

    Tools for view

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,48 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField zipField;
+
+    @FXML
+    private TextField fileOptions;
+
+    @FXML
+    private TextField outDirectory;
+
+    @FXML
+    private Button zipButton;
+
+    @FXML
+    void pathToZip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pathToOptions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pathOutDirectory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
