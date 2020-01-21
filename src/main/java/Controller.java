643eef9b4e56968f11dcb5a2e3ae91aded9bdef3
Shodgearo <sanya.93.26@bk.ru>
2020-01-21 11:19:15 +0600

    Add apache poi maven and file and directory choosers

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -5,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 public class Controller {
 
@@ -28,17 +31,31 @@ public class Controller {
 
     @FXML
     void pathToZip(ActionEvent event) {
+        FileChooser fc = new FileChooser();
 
+        File file = fc.showOpenDialog(null);
+
+        if (file != null)
+            zipField.setText(file.getAbsolutePath());
     }
 
     @FXML
     void pathToOptions(ActionEvent event) {
+        FileChooser fc = new FileChooser();
 
+        File file = fc.showOpenDialog(null);
+
+        if (file != null)
+            fileOptions.setText(file.getAbsolutePath());
     }
 
     @FXML
     void pathOutDirectory(ActionEvent event) {
+        DirectoryChooser dc = new DirectoryChooser();
 
+        File file = dc.showDialog(null);
+
+        outDirectory.setText(file.getAbsolutePath());
     }
 
     @FXML
